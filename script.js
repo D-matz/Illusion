46ae1458259ff9c576f0c200d4f4b51fac45e45b
David Matz <davidmatz@cs.uchicago.edu>
2019-12-23 00:26:00 -0600

    intellij?

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("boo");
+function myFunction() {
+  document.getElementById("myRedDIV").style.order = "4";
+  document.getElementById("myBlueDIV").style.order = "3";
+  document.getElementById("myGreenDIV").style.order = "1";
+  document.getElementById("myPinkDIV").style.order = "2";
+}
